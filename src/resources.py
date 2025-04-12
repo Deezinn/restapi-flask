1b0f279fb5b83a371e967323137ae1ee4002e552
Deezinn <developer.andrelz@gmail.com>
2025-04-12 18:38:17 -0300

    add new routes

--- src/resources.py
+++ src/resources.py
@@ -3,3 +3,14 @@ from flask_restful import Resource
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
+
+class Users(Resource):
+    def get(self):
+        return {'user': 'André'}
+
+class User(Resource):
+    def get(self):
+        return {'message': 'Teste'}
+
+    def get(self, cpf):
+        return {'message': cpf}
--- src/routes.py
+++ src/routes.py
@@ -1,4 +1,6 @@
 from .app import api
-from .resources import HelloWorld
+from .resources import HelloWorld, Users, User
 
 api.add_resource(HelloWorld, '/')
+api.add_resource(Users, '/users')
+api.add_resource(User, '/user', '/user/<string:cpf>')
