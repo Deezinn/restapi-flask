3ead7b5d8dcc74b922ece8883ebd18c0bd7b51c1
Deezinn <developer.andrelz@gmail.com>
2025-04-12 18:17:11 -0300

    create index and app

---
+++ app/index.py
@@ -0,0 +1,11 @@
+from .app import app, api
+from flask_restful import Resource
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
