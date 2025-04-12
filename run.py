18646333f15287a19945b5ce2828650799de5a9c
Deezinn <developer.andrelz@gmail.com>
2025-04-12 18:55:13 -0300

    fix: formatting according flake8

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from src.app import app
-import src.routes
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- src/resources.py
+++ src/resources.py
@@ -1,16 +1,19 @@
 from flask_restful import Resource
 
+
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
+
 
 class Users(Resource):
     def get(self):
         return {'user': 'André'}
 
+
 class User(Resource):
     def get(self, cpf):
         return {'message': cpf}
-    
+
     def post(self):
-        return {'message': 'Teste'}
+        return {'message': 'teste'}
